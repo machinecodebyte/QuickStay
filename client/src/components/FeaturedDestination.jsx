205a615cddcf0cc23f4cb756293a9700d09dce48
Haseeb Ahmed <harrypotterfromhogwartzz@gmail.com>
2026-01-06 02:57:23 +0530

    Fixed the responsive issue in Featured Destination component on a Home Page

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -14,15 +14,17 @@ const FeaturedDestination = () => {
         subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalled luxury and unforgettable experiences"
       />
 
-      <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-        {roomsDummyData.slice(0.4).map((room, index) => (
+      {/* FIXED GRID */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-20">
+        {roomsDummyData.slice(0, 4).map((room, index) => (
           <HotelCard key={room._id} room={room} index={index} />
         ))}
       </div>
+
       <button
         onClick={() => {
           navigate("/rooms");
-          scrollTo(0,0);
+          scrollTo(0, 0);
         }}
         className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer"
       >
